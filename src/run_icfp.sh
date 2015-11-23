ec5bfb63d37b6dff3854c68cbf42c9b51f404150
Abhishek Udupa <audupa@seas.upenn.edu>
2015-11-23 14:31:44 -0500

    Minor fix

--- src/run_icfp.sh
+++ src/run_icfp.sh
@@ -6,7 +6,7 @@ rm -f log
 for file in ../benchmarks/icfp/*.sl; do
 	echo "====================" >> log
 	echo "START BENCHMARK: $file" >> log
-	PYTHONPATH=$PYTHONPATH:../thirdparty/libeusolver/build/:../thirdparty/z3/build/:../thirdparty/bitstring-3.1.3/ timeout "$1" python3 solvers.py icfp $file log}
+	PYTHONPATH=$PYTHONPATH:../thirdparty/libeusolver/build/:../thirdparty/z3/build/:../thirdparty/bitstring-3.1.3/ timeout "$1" python3 solvers.py icfp $file log
 	echo "END BENCHMARK: $file" >> log
 	echo "====================" >> log
 done
