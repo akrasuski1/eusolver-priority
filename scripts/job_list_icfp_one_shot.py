c96dab478fd74611c3e378c27229ef649f956d38
Abhishek Udupa <audupa@seas.upenn.edu>
2016-01-26 16:49:56 -0500

    Added job lists for icfp

---
+++ scripts/job_list_icfp_one_shot.py
@@ -0,0 +1,94 @@
+# job_list_one_shot.py ---
+#
+# Filename: job_list_one_shot.py
+# Author: Abhishek Udupa
+# Created: Tue Jan 26 15:13:19 2016 (-0500)
+#
+#
+# Copyright (c) 2015, Abhishek Udupa, University of Pennsylvania
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. All advertising materials mentioning features or use of this software
+#    must display the following acknowledgement:
+#    This product includes software developed by The University of Pennsylvania
+# 4. Neither the name of the University of Pennsylvania nor the
+#    names of its contributors may be used to endorse or promote products
+#    derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER ''AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+#
+
+# Code:
+
+[
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_103_10.sl'], 'icfp_103_10-one-shot', 'icfp_103_10-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_113_1000.sl'], 'icfp_113_1000-one-shot', 'icfp_113_1000-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_125_10.sl'], 'icfp_125_10-one-shot', 'icfp_125_10-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_14_1000.sl'], 'icfp_14_1000-one-shot', 'icfp_14_1000-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_147_1000.sl'], 'icfp_147_1000-one-shot', 'icfp_147_1000-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_28_10.sl'], 'icfp_28_10-one-shot', 'icfp_28_10-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_39_100.sl'], 'icfp_39_100-one-shot', 'icfp_39_100-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_51_10.sl'], 'icfp_51_10-one-shot', 'icfp_51_10-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_68_1000.sl'], 'icfp_68_1000-one-shot', 'icfp_68_1000-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_72_10.sl'], 'icfp_72_10-one-shot', 'icfp_72_10-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_82_10.sl'], 'icfp_82_10-one-shot', 'icfp_82_10-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_94_1000.sl'], 'icfp_94_1000-one-shot', 'icfp_94_1000-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_96_10.sl'], 'icfp_96_10-one-shot', 'icfp_96_10-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_104_10.sl'], 'icfp_104_10-one-shot', 'icfp_104_10-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_114_100.sl'], 'icfp_114_100-one-shot', 'icfp_114_100-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_134_1000.sl'], 'icfp_134_1000-one-shot', 'icfp_134_1000-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_143_1000.sl'], 'icfp_143_1000-one-shot', 'icfp_143_1000-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_150_10.sl'], 'icfp_150_10-one-shot', 'icfp_150_10-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_30_10.sl'], 'icfp_30_10-one-shot', 'icfp_30_10-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_45_1000.sl'], 'icfp_45_1000-one-shot', 'icfp_45_1000-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_54_1000.sl'], 'icfp_54_1000-one-shot', 'icfp_54_1000-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_69_10.sl'], 'icfp_69_10-one-shot', 'icfp_69_10-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_73_10.sl'], 'icfp_73_10-one-shot', 'icfp_73_10-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_87_10.sl'], 'icfp_87_10-one-shot', 'icfp_87_10-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_94_100.sl'], 'icfp_94_100-one-shot', 'icfp_94_100-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_99_100.sl'], 'icfp_99_100-one-shot', 'icfp_99_100-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_105_1000.sl'], 'icfp_105_1000-one-shot', 'icfp_105_1000-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_118_100.sl'], 'icfp_118_100-one-shot', 'icfp_118_100-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_135_100.sl'], 'icfp_135_100-one-shot', 'icfp_135_100-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_144_1000.sl'], 'icfp_144_1000-one-shot', 'icfp_144_1000-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_21_1000.sl'], 'icfp_21_1000-one-shot', 'icfp_21_1000-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_32_10.sl'], 'icfp_32_10-one-shot', 'icfp_32_10-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_45_10.sl'], 'icfp_45_10-one-shot', 'icfp_45_10-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_56_1000.sl'], 'icfp_56_1000-one-shot', 'icfp_56_1000-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_7_1000.sl'], 'icfp_7_1000-one-shot', 'icfp_7_1000-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_81_1000.sl'], 'icfp_81_1000-one-shot', 'icfp_81_1000-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_9_1000.sl'], 'icfp_9_1000-one-shot', 'icfp_9_1000-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_95_100.sl'], 'icfp_95_100-one-shot', 'icfp_95_100-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_105_100.sl'], 'icfp_105_100-one-shot', 'icfp_105_100-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_118_10.sl'], 'icfp_118_10-one-shot', 'icfp_118_10-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_139_10.sl'], 'icfp_139_10-one-shot', 'icfp_139_10-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_144_100.sl'], 'icfp_144_100-one-shot', 'icfp_144_100-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_25_1000.sl'], 'icfp_25_1000-one-shot', 'icfp_25_1000-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_38_10.sl'], 'icfp_38_10-one-shot', 'icfp_38_10-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_5_1000.sl'], 'icfp_5_1000-one-shot', 'icfp_5_1000-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_64_10.sl'], 'icfp_64_10-one-shot', 'icfp_64_10-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_7_10.sl'], 'icfp_7_10-one-shot', 'icfp_7_10-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_82_100.sl'], 'icfp_82_100-one-shot', 'icfp_82_100-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_93_1000.sl'], 'icfp_93_1000-one-shot', 'icfp_93_1000-one-shot'),
+    (['python3', 'solvers.py', '3600', 'icfp', '../benchmarks/icfp/icfp_96_1000.sl'], 'icfp_96_1000-one-shot', 'icfp_96_1000-one-shot')
+]
+
+#
+# job_list_one_shot.py ends here
